d5a723dff37548dfc1adcf976b1f5fba83fcfaa5
Ardli FW <ardli.wangsaatmaja@binus.ac.id>
2023-04-22 16:06:34 +0700

    loading view color fix

--- Object Color Picker.swiftpm/LoadingView.swift
+++ Object Color Picker.swiftpm/LoadingView.swift
@@ -3,7 +3,6 @@ import SwiftUI
 struct LoadingView: View {
     var body: some View {
         ProgressView()
-            .progressViewStyle(CircularProgressViewStyle(tint: Color.white))
             .padding(.leading, 4)
     }
 }
